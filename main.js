ba9962b51ef247563476590b2491bdcf25a8e27a
Diego Oliveira <diegolvr14@gmail.com>
2022-08-17 17:35:46 -0300

    Continuação da atividade ES6

--- main.js
+++ main.js
@@ -1,71 +1,99 @@
-//Classes com herança
+const arr = [1,2,3,6,8,9]
 
-class List{
-    constructor(){
-        this.data = []
-    }
+const newArr = arr.map(function(item){
+    return item * 2;
+})
+console.log(newArr)
+
+const soma1 = arr.reduce(function(total, next){
+    return total + next;
+} )
+console.log(soma)
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+})
+console.log(filter)
+
+const find = arr.filter(item => item === 3)  //funcão reduzida em relacão as anteriores
+console.log(find)
+
+const teste = () => {  return {nome:'Lucas'}}
+console.log(teste())
+
+function soma(a=2,b=3){
+    return a+b;
+}
+console.log(soma(3))
+
+// //Classes com herança
+
+// class List{
+//     constructor(){
+//         this.data = []
+//     }
     
-    add(nome) {
-        this.data.push(nome);
-        console.log(this.data);
+//     add(nome) {
+//         this.data.push(nome);
+//         console.log(this.data);
 
-    }
-}
-
-class ToList extends List{
-    constructor() {
-        super();
-        this.usuario = 'Lucas';
-    } 
-
-    getUsuario(){
-        console.log(this.usuario);
-    }
-
-    static soma(a,b){
-        return a+b;
-        //return this.usuario;
-    }
-}
-
-//instancia a classe
-
-// const tolist = new ToList()
-
-//adiciona ao array data a string "adicionei" cada vez que clica no botao e chama o metodo getUsuario
-
-// document.getElementById('novotodo').onclick = function () {
-//         tolist.add('Joao');
-//         tolist.getUsuario();
+//     }
 // }
 
+// class ToList extends List{
+//     constructor() {
+//         super();
+//         this.usuario = 'Lucas';
+//     } 
 
-//chama o metodo estático
+//     getUsuario(){
+//         console.log(this.usuario);
+//     }
 
-//console.log(ToList.soma(1,2))
+//     static soma(a,b){
+//         return a+b;
+//         //return this.usuario;
+//     }
+// }
 
-//var nome = "Lucas"; //A var pode alterar receber o nome, já o cost não aceita alteracão
-const nome = {primeiro_nome: "Lucas"}
+// //instancia a classe
 
-nome.primeiro_nome = "Joao" //Utilizando esse método da pra alterar o nome
+// // const tolist = new ToList()
 
-console.log(nome);
+// //adiciona ao array data a string "adicionei" cada vez que clica no botao e chama o metodo getUsuario
 
-function teste(x){
-    let y = 4
-    if(x > 2){
-        y = 6
-        console.log(x,y)
-    }
+// // document.getElementById('novotodo').onclick = function () {
+// //         tolist.add('Joao');
+// //         tolist.getUsuario();
+// // }
 
-    else{
-        console.log(y)  //testando um else pra ver se eu ainda sei
-    }
-}
 
-console.log(teste(10))
-console.log(teste(1))
-//console.log(teste(y)) // o Y só está visivel dentro do escopo da funcao "Teste"
+// //chama o metodo estático
+
+// //console.log(ToList.soma(1,2))
+
+// //var nome = "Lucas"; //A var pode alterar receber o nome, já o cost não aceita alteracão
+// const nome = {primeiro_nome: "Lucas"}
+
+// nome.primeiro_nome = "Joao" //Utilizando esse método da pra alterar o nome
+
+// console.log(nome);
+
+// function teste(x){
+//     let y = 4
+//     if(x > 2){
+//         y = 6
+//         console.log(x,y)
+//     }
+
+//     else{
+//         console.log(y)  //testando um else pra ver se eu ainda sei
+//     }
+// }
+
+// console.log(teste(10))
+// console.log(teste(1))
+// //console.log(teste(y)) // o Y só está visivel dentro do escopo da funcao "Teste"
 
 
 
